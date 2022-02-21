be1718e7781ec4e308429cbe651ff964d163b547
socialdistance <pleaseuseurbrain@protonmail.com>
2022-02-21 15:54:08 +0300

    HW10 is completed

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,12 +1,11 @@
 package hw10programoptimization
 
 import (
-	"encoding/json"
-	"fmt"
+	"bufio"
 	"io"
-	"io/ioutil"
-	"regexp"
 	"strings"
+
+	"github.com/mailru/easyjson"
 )
 
 type User struct {
@@ -21,47 +20,78 @@ type User struct {
 
 type DomainStat map[string]int
 
-func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return countDomains(u, domain)
-}
+func GetDomainStatNew(r io.Reader, domain string) (DomainStat, error) {
+	domainStat := make(DomainStat)
+	domainStatDot := "." + strings.ToLower(domain)
 
-type users [100_000]User
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanLines)
 
-func getUsers(r io.Reader) (result users, err error) {
-	content, err := ioutil.ReadAll(r)
-	if err != nil {
-		return
-	}
-
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
-			return
+	for scanner.Scan() {
+		res := scanner.Text()
+		if !strings.Contains(res, domain) {
+			continue
 		}
-		result[i] = user
-	}
-	return
-}
 
-func countDomains(u users, domain string) (DomainStat, error) {
-	result := make(DomainStat)
-
-	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
+		user := &User{}
+		err := easyjson.Unmarshal([]byte(res), user)
 		if err != nil {
 			return nil, err
 		}
 
-		if matched {
-			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
-			num++
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
+		domain := strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])
+		if !strings.Contains(domain, domainStatDot) {
+			continue
 		}
+
+		domainStat[domain]++
 	}
-	return result, nil
+
+	return domainStat, nil
 }
+
+func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
+	return GetDomainStatNew(r, domain)
+	// u, err := getUsers(r)
+	// if err != nil {
+	//	return nil, fmt.Errorf("get users error: %w", err)
+	// }
+	// return countDomains(u, domain)
+}
+
+// type users [100_000]User
+//
+// func getUsers(r io.Reader) (result users, err error) {
+//	content, err := ioutil.ReadAll(r)
+//	if err != nil {
+//		return
+//	}
+//
+//	lines := strings.Split(string(content), "\n")
+//	for i, line := range lines {
+//		var user User
+//		if err = json.Unmarshal([]byte(line), &user); err != nil {
+//			return
+//		}
+//		result[i] = user
+//	}
+//	return
+//}
+//
+// func countDomains(u users, domain string) (DomainStat, error) {
+//	result := make(DomainStat)
+//
+//	for _, user := range u {
+//		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
+//		if err != nil {
+//			return nil, err
+//		}
+//
+//		if matched {
+//			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
+//			num++
+//			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
+//		}
+//	}
+//	return result, nil
+//}
--- hw10_program_optimization/stats_test.go
+++ hw10_program_optimization/stats_test.go
@@ -1,3 +1,4 @@
+//go:build !bench
 // +build !bench
 
 package hw10programoptimization
@@ -9,12 +10,44 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func BenchmarkGetDomainStat(b *testing.B) {
+	data := `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
+{"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
+{"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
+{"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
+
+	for i := 0; i < b.N; i++ {
+		_, _ = GetDomainStat(bytes.NewBufferString(data), "gov")
+	}
+}
+
 func TestGetDomainStat(t *testing.T) {
 	data := `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
 {"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
 {"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
 {"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
 {"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
+
+	dataError := `{{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}`
+
+	dataAndError := `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+{{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}`
+
+	t.Run("data with error", func(t *testing.T) {
+		_, err := GetDomainStat(bytes.NewBufferString(dataError), "gov")
+		require.Error(t, err)
+	})
+
+	t.Run("error test", func(t *testing.T) {
+		_, err := GetDomainStat(bytes.NewBufferString(dataAndError), "gov")
+		require.Error(t, err)
+	})
+
+	t.Run("no error", func(t *testing.T) {
+		_, err := GetDomainStat(bytes.NewBufferString(dataAndError), "com")
+		require.NoError(t, err)
+	})
 
 	t.Run("find 'com'", func(t *testing.T) {
 		result, err := GetDomainStat(bytes.NewBufferString(data), "com")
